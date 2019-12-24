75340ce3c460a32388ae48a441cc649f7f445bb2
errabelli09 <anilraoerrabelli@gmail.com>
2019-12-24 16:27:39 +0530

    Initial Commit
    Initial Commit

---
+++ SDPWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDPWeather
+//
+//  Created by Errabelli Anil on 24/12/19.
+//  Copyright © 2019 Errabelli Anil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
