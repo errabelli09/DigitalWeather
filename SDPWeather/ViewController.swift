6215a78275254d26ddbb2857ff7cef16ab184236
errabelli09 <anilraoerrabelli@gmail.com>
2019-12-24 18:23:20 +0530

    HomeTableviewController added with SearchBar

--- SDPWeather/ViewController.swift
+++ SDPWeather/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let apiKeyUrl = "http://api.worldweatheronline.com/premium/v1/search.ashx?query=london&num_of_results=10&format=json&key=e6b8915fb40d45dd93b50608192412"
     }
 
 
