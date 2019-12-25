f778fe9e369cfd9ff64b272c1f94fffc16c948fc
Anil Errabelli <anilraoerrabelli@gmail.com>
2019-12-26 03:46:48 +0530

    Search Api Implementation
    SearchBar added and Aearch api implementation

--- SDPWeather/HomeTableViewController.swift
+++ SDPWeather/HomeTableViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
-class HomeTableViewController: UITableViewController {
+class HomeTableViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    //MARK:- Outlets
+    
+    
+    //MARK:- Properties
+    
+    var filteredArray = [[String:Any]]()
+    var shouldShowSearchResults = false
+    var searchController: UISearchController!
 
-    @IBOutlet weak var searchBar: UISearchBar!
+    //MARK:- ViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,29 +29,129 @@ class HomeTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        configureSearchController() // Config Controller in VC
+        
+        loadListOfCountries()
+        
     }
+    
+    //MARK:- VC Methods
+
+        func loadListOfCountries() {
+//            // Specify the path to the countries list file.
+//            let pathToFile = Bundle.main.path(forResource: "Country", ofType: "txt")
+//
+//            if let path = pathToFile {
+//                // Load the file contents as a string.
+//
+//                do{
+//                    let countriesString = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+//                    self.dataArray = countriesString.components(separatedBy: "\n")
+//                }
+//                catch{
+//                    print("try-catch error is catched!!")
+//                }
+//                tableView.reloadData()
+//            }
+
+            
+            let session = URLSession.shared
+            let url = URL(string: "http://api.worldweatheronline.com/premium/v1/search.ashx?query=london&num_of_results=10&format=json&key=e6b8915fb40d45dd93b50608192412")!
+
+            let task = session.dataTask(with: url) { data, response, error in
+
+                if error != nil || data == nil {
+                    print("Client error!")
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                    print("Server error!")
+                    return
+                }
+
+                guard let mime = response.mimeType, mime == "application/json" else {
+                    print("Wrong MIME type!")
+                    return
+                }
+
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
+                    
+                    self.filteredArray = json.value(forKeyPath: "search_api.result") as! [[String:Any]]
+                    
+                    DispatchQueue.main.async {
+                        //Do UI Code here.
+                        self.tableView.reloadData()
+                    }
+
+                    print(json)
+                } catch {
+                    print("JSON error: \(error.localizedDescription)")
+                }
+            }
+
+            task.resume()
+        }
+
+
+        func configureSearchController() {
+            searchController = UISearchController(searchResultsController: nil)
+            searchController.dimsBackgroundDuringPresentation = false
+            searchController.searchBar.placeholder = "Search here..."
+            searchController.searchBar.delegate = self
+            searchController.searchResultsUpdater = self
+            searchController.searchBar.sizeToFit()
+
+            tableView.tableHeaderView = searchController.searchBar
+        }
+    
+    //MARK:- search update delegate
+
+    public func updateSearchResults(for searchController: UISearchController){
+        let searchString = searchController.searchBar.text
+
+        tableView.reloadData()
+    }
+
+    //MARK:- search bar delegate
+    //MARK:-
+
+    public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+
+
+    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return filteredArray.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let  areaArray = filteredArray[0]["areaName"] as! [[String:Any]]
 
-        // Configure the cell...
+        cell.textLabel?.text = areaArray[0]["value"] as? String
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
--- SDPWeather/ViewController.swift
+++ SDPWeather/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         let apiKeyUrl = "http://api.worldweatheronline.com/premium/v1/search.ashx?query=london&num_of_results=10&format=json&key=e6b8915fb40d45dd93b50608192412"
     }
 
-
+//https://stackoverflow.com/questions/24330056/how-to-throttle-search-based-on-typing-speed-in-ios-uisearchbar
+//https://stackoverflow.com/questions/51699166/swift-4-which-type-of-array-to-use-parse-json-response-into-array
 }
 
